ce5d1e85bc6afe503d609b4f8dbe8c35bb5e71ee
jihadul-islam <jihadulislam708@gmail.com>
2020-08-11 14:12:05 -0800

    last-commit

---
+++ assignment.js
@@ -0,0 +1,35 @@
+
+
+
+//feetToMile
+function feetToMile(feet) {
+    var mile = feet /5280
+    return mile;
+}
+var result = feetToMile(10560);
+console.log(result);
+
+//woodCalculator
+
+
+function woodCalculator(table,chair,bed) {
+    var tableWood = table * 5;
+    var chairWood = chair *4;
+    var bedWood = bed * 4;
+    var totalWood = tableWood+chairWood+bedWood;
+    return totalWood;
+    
+}
+
+
+//brickCalculator
+function brickCalculator() {
+    
+}
+
+//tainyFriend
+function tainyFriend(params) {
+    var friend =["minhaz","kobir","kulsum","airin",]
+    var tainyfriend=friend.length;
+    
+}
